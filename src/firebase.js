452b40b023a553cd1410016d7005100351664348
tanmay goyal <tanmaygoyal888@gmail.com>
2022-08-26 10:08:39 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -12,14 +12,13 @@
   import firebase from 'firebase'
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCH9tZTmAhNmMs1_QbVKAO4JTu1ksjC7MI",
-    authDomain: "todo-app-30495.firebaseapp.com",
-    databaseURL: "https://todo-app-30495.firebaseio.com",
-    projectId: "todo-app-30495",
-    storageBucket: "todo-app-30495.appspot.com",
-    messagingSenderId: "559639778604",
-    appId: "1:559639778604:web:7bcbec5c1c0f41ed870889",
-    measurementId: "G-2EPRGB3YM3"
+    apiKey: "AIzaSyB4U4CN1BHByOajejkvxLjGmbL7rSzYcfg",
+  authDomain: "shellcode-9fa78.firebaseapp.com",
+  projectId: "shellcode-9fa78",
+  storageBucket: "shellcode-9fa78.appspot.com",
+  messagingSenderId: "372248540792",
+  appId: "1:372248540792:web:d2c6282e77e58c60149f8d",
+  measurementId: "G-K4ZMPJWR2Y"
   })
 
   const db = firebaseApp.firestore();
